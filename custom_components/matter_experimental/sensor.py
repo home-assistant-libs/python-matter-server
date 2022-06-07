97b4a848730652953ffbd3b38f27cabcca25a1fc
Paulus Schoutsen <balloob@gmail.com>
2022-06-07 15:01:38 -0700

    Support Light Sensor

--- custom_components/matter_experimental/sensor.py
+++ custom_components/matter_experimental/sensor.py
@@ -9,7 +9,14 @@ from homeassistant.components.sensor import (
     SensorStateClass,
 )
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform, TEMP_CELSIUS
+from homeassistant.const import (
+    Platform,
+    TEMP_CELSIUS,
+    PERCENTAGE,
+    LIGHT_LUX,
+    VOLUME_FLOW_RATE_CUBIC_METERS_PER_HOUR,
+    PRESSURE_KPA,
+)
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
@@ -73,7 +80,7 @@ class MatterPressureSensor(MatterSensor):
     """Representation of a Matter pressure sensor."""
 
     _attr_device_class = SensorDeviceClass.PRESSURE
-    _attr_native_unit_of_measurement = "kPa"
+    _attr_native_unit_of_measurement = PRESSURE_KPA
 
     @callback
     def _update_from_device(self) -> None:
@@ -93,7 +100,7 @@ class MatterPressureSensor(MatterSensor):
 class MatterFlowSensor(MatterSensor):
     """Representation of a Matter flow sensor."""
 
-    _attr_native_unit_of_measurement = "m³/h"
+    _attr_native_unit_of_measurement = VOLUME_FLOW_RATE_CUBIC_METERS_PER_HOUR
 
     @callback
     def _update_from_device(self) -> None:
@@ -112,7 +119,7 @@ class MatterHumiditySensor(MatterSensor):
     """Representation of a Matter humidity sensor."""
 
     _attr_device_class = SensorDeviceClass.HUMIDITY
-    _attr_native_unit_of_measurement = "%"
+    _attr_native_unit_of_measurement = PERCENTAGE
 
     @callback
     def _update_from_device(self) -> None:
@@ -125,6 +132,27 @@ class MatterHumiditySensor(MatterSensor):
             measurement = None
         else:
             measurement /= 100
+
+        self._attr_native_value = measurement
+
+
+class MatterLightSensor(MatterSensor):
+    """Representation of a Matter light sensor."""
+
+    _attr_device_class = SensorDeviceClass.ILLUMINANCE
+    _attr_native_unit_of_measurement = LIGHT_LUX
+
+    @callback
+    def _update_from_device(self) -> None:
+        """Update from device."""
+        measurement = self._device.get_cluster(
+            clusters.RelativeHumidityMeasurement
+        ).measuredValue
+
+        if measurement is NullValue:
+            measurement = None
+        else:
+            measurement = round(pow(10, ((measurement - 1) / 10000)), 1)
 
         self._attr_native_value = measurement
 
@@ -150,6 +178,12 @@ DEVICE_ENTITY: dict[
         entity_cls=MatterHumiditySensor,
         subscribe_attributes=(
             clusters.RelativeHumidityMeasurement.Attributes.MeasuredValue,
+        ),
+    ),
+    matter_devices.LightSensor: DeviceMapping(
+        entity_cls=MatterLightSensor,
+        subscribe_attributes=(
+            clusters.IlluminanceMeasurement.Attributes.MeasuredValue,
         ),
     ),
 }
