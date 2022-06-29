87e39e533cc2529a61e51cace02aa5e059c97af6
Paulus Schoutsen <balloob@gmail.com>
2022-06-29 15:52:53 -0700

    Extract device registry tests

--- tests/matter_experimental/test_mapping.py
+++ tests/matter_experimental/test_mapping.py
@@ -1,91 +1,38 @@
 """Test that fixtures result in right data in Home Assistant."""
 from __future__ import annotations
 
-import asyncio
 import json
 import logging
 from typing import TYPE_CHECKING
-from unittest.mock import Mock, patch
 
 from homeassistant.helpers import device_registry as dr, entity_registry as er
 import pytest
-from pytest_homeassistant_custom_component.common import MockConfigEntry
 
 from custom_components.matter_experimental.const import DOMAIN
 from custom_components.matter_experimental.device_platform import DEVICE_PLATFORM
-from matter_server.client.client import Client
-from matter_server.client.model.driver import Driver
-from matter_server.client.model.node import MatterNode
-from matter_server.common import json_utils
-from matter_server.vendor.chip.clusters.ObjectsVersion import CLUSTER_OBJECT_VERSION
+from tests.matter_experimental.common import setup_integration_with_node_fixture
 
 from ..fixtures import NODE_FIXTURES_ROOT, NODE_IN_HA_FIXTURES_ROOT
 
-from tests.test_utils.mock_matter import get_mock_matter
 
 if TYPE_CHECKING:
     from homeassistant.core import HomeAssistant
 
 
-MOCK_COMPR_FABRIC_ID = 1234
 LOGGER = logging.getLogger(__name__)
-
-
-class MockClient(Client):
-
-    mock_client_disconnect: asyncio.Event
-
-    async def connect(self):
-        self.server_info = Mock(compressedFabricId=MOCK_COMPR_FABRIC_ID)
-
-    async def listen(self, driver_ready):
-        self.driver = Driver(self)
-        driver_ready.set()
-        self.mock_client_disconnect = asyncio.Event()
-        await self.mock_client_disconnect.wait()
 
 
 @pytest.mark.parametrize("node_fixture", NODE_FIXTURES_ROOT.iterdir())
 async def test_fixture(hass: HomeAssistant, hass_storage, node_fixture):
-    node_data = json.loads(node_fixture.read_text(), cls=json_utils.CHIPJSONDecoder)
-    node = MatterNode(
-        get_mock_matter(),
-        node_data,
+    node = await setup_integration_with_node_fixture(
+        hass, hass_storage, node_fixture.name.rpartition(".")[0]
     )
+
     checks = json.loads((NODE_IN_HA_FIXTURES_ROOT / node_fixture.name).read_text())
 
     assert (node.bridge_device_type_instance is not None) == checks["is_bridge"]
 
     assert len(node.node_devices) == len(checks["node_devices"])
-
-    # Set up HA
-    config_entry = MockConfigEntry(
-        domain="matter_experimental", data={"url": "http://mock-matter-server-url"}
-    )
-    config_entry.add_to_hass(hass)
-
-    storage_key = f"matter_experimental_{config_entry.entry_id}"
-    hass_storage[storage_key] = {
-        "version": 1,
-        "minor_version": 0,
-        "key": storage_key,
-        "data": {
-            "compressed_fabric_id": MOCK_COMPR_FABRIC_ID,
-            "next_node_id": 4339,
-            "node_interview_version": CLUSTER_OBJECT_VERSION,
-            "nodes": {str(node.node_id): node_data},
-        },
-    }
-
-    async def mock_init_matter_device(self):
-        self._update_from_device()
-
-    with patch("matter_server.client.matter.Client", MockClient), patch(
-        "custom_components.matter_experimental.entity.MatterEntity.init_matter_device",
-        mock_init_matter_device,
-    ):
-        assert await hass.config_entries.async_setup(config_entry.entry_id)
-        await hass.async_block_till_done()
 
     dev_reg = dr.async_get(hass)
     ent_reg = er.async_get(hass)
@@ -95,24 +42,9 @@ async def test_fixture(hass: HomeAssistant, hass_storage, node_fixture):
         LOGGER.info("Checking device #%s %s", md_idx, node_device)
         nd_checks = checks["node_devices"][md_idx]
 
-        # Validate device info
-        info = node_device.device_info()
-        for key in "vendorName", "productName", "uniqueID":
-            assert getattr(info, key) == nd_checks[key], key
-
-        # Validate device registry
-        device_entry = dev_reg.async_get_device({(DOMAIN, nd_checks["uniqueID"])})
-        assert device_entry is not None
-        assert device_entry.manufacturer == nd_checks["vendorName"]
-        assert device_entry.model == nd_checks["productName"]
-
-        # Validate via device
-        if node.bridge_device_type_instance:
-            via_device_id = dev_reg.async_get_device({(DOMAIN, node.unique_id)}).id
-        else:
-            via_device_id = None
-
-        assert device_entry.via_device_id == via_device_id
+        device_entry = dev_reg.async_get_device(
+            {(DOMAIN, node_device.device_info().uniqueID)}
+        )
 
         node_entities = {
             ent.entity_id: ent
