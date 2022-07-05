c2e45da743103aa814c980084d5f3b36aeb4c970
Marcelo Borges <35546901+jmmb13@users.noreply.github.com>
2022-07-05 14:41:09 +0100

    Adds argparse to enable log to file (#2)
    * Adds argparse to enable log to file
    
    * Adds log-level to arguments
    
    Co-authored-by: Stefan Agner <stefan@agner.ch>

--- matter_server/server/__main__.py
+++ matter_server/server/__main__.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 import logging
 import os
@@ -9,12 +10,42 @@ import coloredlogs
 from .matter_stack import MatterStack
 from .server import MatterServer
 
-logging.basicConfig(level=logging.WARN)
 _LOGGER = logging.getLogger(__package__)
-_LOGGER.setLevel(logging.DEBUG)
+
+
+def get_arguments() -> argparse.Namespace:
+    """Get parsed passed in arguments."""
+
+    parser = argparse.ArgumentParser(
+        description="Matter Controller Server using WebSockets."
+    )
+    parser.add_argument(
+        "--log-file",
+        type=str,
+        default=None,
+        help="Log file to write to.  If not set, matter_server.log is used",
+    )
+    parser.add_argument(
+        "-log",
+        "--log-level",
+        default="info",
+        help="Provide logging level. Example --log-level debug, default=info, possible=(critical, error, warning, info, debug)",
+    )
+
+    arguments = parser.parse_args()
+
+    return arguments
 
 
 def main() -> int:
+    args = get_arguments()
+    handlers = None
+
+    if args.log_file:
+        handlers = [logging.FileHandler(args.log_file)]
+
+    logging.basicConfig(handlers=handlers, level=args.log_level.upper())
+
     host = os.getenv("CHIP_WS_SERVER_HOST", "::,0.0.0.0").split(",")
     port = int(os.getenv("CHIP_WS_SERVER_PORT", "5580"))
     storage_path = os.getenv(
@@ -23,7 +54,7 @@ def main() -> int:
     )
     debug = os.getenv("CHIP_WS_DEBUG") is not None
 
-    coloredlogs.install(level=logging.DEBUG if debug else logging.INFO)
+    coloredlogs.install(level=logging.DEBUG if debug else args.log_level.upper())
     stack = MatterStack(storage_path)
 
     loop = asyncio.get_event_loop()
