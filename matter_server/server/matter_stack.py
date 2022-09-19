9ad2231ff1c6f0ff2a629c625c15c39635e11463
Stefan Agner <stefan@agner.ch>
2022-09-19 18:12:42 +0200

    Initialize Matter stack correctly

--- matter_server/server/matter_stack.py
+++ matter_server/server/matter_stack.py
@@ -14,16 +14,26 @@ class MatterStack:
     # To track if wifi credentials set this session.
     wifi_cred_set = False
     device_controller: ChipDeviceController
+    certificate_authority_manager = None
 
     def __init__(self, storage_path):
         _LOGGER.info("Setup CHIP Controller Server")
-        chip.native.GetLibraryHandle()
+        chip.native.Init()
         chip.logging.RedirectToPythonLogging()
-        self.stack = ChipStack(persistentStoragePath=storage_path)
-        self.fabric_admin = FabricAdmin()
-        self.device_controller = self.fabric_admin.NewController()
+        self.stack = ChipStack(persistentStoragePath=storage_path, enableServerInteractions=False)
+        self.certificate_authority_manager = chip.CertificateAuthority.CertificateAuthorityManager(self.stack, self.stack.GetStorageManager())
+
+        self.certificate_authority_manager.LoadAuthoritiesFromStorage()
+
+        if (len(self.certificate_authority_manager.activeCaList) == 0):
+            ca = certificate_authority_manager.NewCertificateAuthority()
+            ca.NewFabricAdmin(vendorId=0xFFF1, fabricId=1)
+        elif (len(self.certificate_authority_manager.activeCaList[0].adminList) == 0):
+            self.certificate_authority_manager.activeCaList[0].NewFabricAdmin(vendorId=0xFFF1, fabricId=1)
+
+        self.device_controller = self.certificate_authority_manager.activeCaList[0].adminList[0].NewController()
         _LOGGER.info("CHIP Controller Stack initialized")
-        self.fabric_id = self.device_controller.GetFabricId()
+        self.fabric_id = self.device_controller.fabricId
         self.compressed_fabric_id = self.device_controller.GetCompressedFabricId()
 
     def shutdown(self):
