dd9f882effd7229721eae0b6a0a757098444e3f7
Paulus Schoutsen <balloob@gmail.com>
2022-09-26 13:07:02 -0400

    Always send transitionTime when sending MoveToLevelWithOnOff (#81)

--- scripts/show_stored_node.py
+++ scripts/show_stored_node.py
@@ -1,6 +1,7 @@
 """Show mappings for given JSON."""
 from __future__ import annotations
 
+import asyncio
 import dataclasses
 import json
 import os
@@ -12,7 +13,7 @@ from custom_components.matter_experimental.device_platform import DEVICE_PLATFOR
 from matter_server.client.model.node import MatterNode
 from matter_server.common import json_utils
 
-from tests.test_utils.mock_matter import get_mock_matter
+from tests.matter_experimental.common import get_mock_matter
 
 if TYPE_CHECKING:
     from matter_server.client.model.device_type_instance import MatterDeviceTypeInstance
@@ -125,10 +126,10 @@ def nodes_from_data(data):
 
 
 def get_nodes():
-    mock_matter = get_mock_matter()
+    matter = asyncio.run(get_mock_matter())
 
     return [
-        MatterNode(mock_matter, node_data)
+        MatterNode(matter, node_data)
         for node_data in nodes_from_data(parse_data(resolve_input()))
     ]
 
--- tests/matter_experimental/common.py
+++ tests/matter_experimental/common.py
@@ -1,32 +1,45 @@
 from __future__ import annotations
 
+import asyncio
 from functools import cache
 import json
-import asyncio
-from typing import TYPE_CHECKING
+import logging
+from typing import TYPE_CHECKING, Any
 from unittest.mock import Mock, patch
+
+import pytest
+from pytest_homeassistant_custom_component.common import MockConfigEntry
 
 from matter_server.client.client import Client
 from matter_server.client.model.driver import Driver
-from tests.fixtures import NODE_FIXTURES_ROOT
-from pytest_homeassistant_custom_component.common import MockConfigEntry
 from matter_server.client.model.node import MatterNode
-
 from matter_server.common import json_utils
+from matter_server.common.model.message import ServerInformation
 from matter_server.vendor.chip.clusters.ObjectsVersion import CLUSTER_OBJECT_VERSION
 
-from tests.test_utils.mock_matter import get_mock_matter
+from tests.fixtures import NODE_FIXTURES_ROOT
 
 if TYPE_CHECKING:
     from homeassistant.core import HomeAssistant
 
 
+MOCK_FABRIC_ID = 12341234
 MOCK_COMPR_FABRIC_ID = 1234
 
 
 class MockClient(Client):
 
     mock_client_disconnect: asyncio.Event
+    mock_commands: dict[type, Any] = {}
+    mock_sent_commands = []
+
+    def __init__(self) -> None:
+        super().__init__("mock-url", None)
+        self.mock_commands: dict[type, Any] = {}
+        self.mock_sent_commands = []
+        self.server_info = ServerInformation(
+            fabricId=MOCK_FABRIC_ID, compressedFabricId=MOCK_COMPR_FABRIC_ID
+        )
 
     async def connect(self):
         self.server_info = Mock(compressedFabricId=MOCK_COMPR_FABRIC_ID)
@@ -36,6 +49,50 @@ class MockClient(Client):
         driver_ready.set()
         self.mock_client_disconnect = asyncio.Event()
         await self.mock_client_disconnect.wait()
+
+    def mock_command(self, command_type: type, response: Any) -> None:
+        """Mock a command."""
+        self.mock_commands[command_type] = response
+
+    async def async_send_command(
+        self,
+        command: str,
+        args: dict[str, Any],
+        require_schema: int | None = None,
+    ) -> dict:
+        """Wrapper to send a command to allow mocking commands."""
+        if command == "device_controller.SendCommand" and (
+            (cmd_type := type(args.get("payload"))) in self.mock_commands
+        ):
+            self.mock_sent_commands.append(args)
+            return self.mock_commands[cmd_type]
+
+        return await super().async_send_command(command, args, require_schema)
+
+    async def async_send_command_no_wait(
+        self, command: str, args: dict[str, Any], require_schema: int | None = None
+    ) -> None:
+        """Send a command without waiting for the response."""
+        if command == "SendCommand" and (
+            (cmd_type := type(args.get("payload"))) in self.mock_commands
+        ):
+            self.mock_sent_commands.append(args)
+            return self.mock_commands[cmd_type]
+
+        return await super().async_send_command_no_wait(command, args, require_schema)
+
+
+@pytest.fixture
+async def mock_matter():
+    """Mock matter fixture."""
+    return await get_mock_matter()
+
+
+async def get_mock_matter():
+    """Get mock Matter."""
+    return Mock(
+        adapter=Mock(logger=logging.getLogger("mock_matter")), client=MockClient()
+    )
 
 
 @cache
@@ -54,7 +111,7 @@ async def setup_integration_with_node_fixture(
     """Set up Matter integration with fixture as node."""
     node_data = load_and_parse_node_fixture(node_fixture)
     node = MatterNode(
-        get_mock_matter(),
+        await get_mock_matter(),
         node_data,
     )
     config_entry = MockConfigEntry(
@@ -78,7 +135,9 @@ async def setup_integration_with_node_fixture(
     async def mock_init_matter_device(self):
         self._update_from_device()
 
-    with patch("matter_server.client.matter.Client", MockClient), patch(
+    with patch(
+        "matter_server.client.matter.Client", return_value=node.matter.client
+    ), patch(
         "custom_components.matter_experimental.entity.MatterEntity.init_matter_device",
         mock_init_matter_device,
     ):
