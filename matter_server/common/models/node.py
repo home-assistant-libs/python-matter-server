9a3645b4c025f7894d286f53b5060943ed3da3d9
Martin Hjelmare <marhje52@gmail.com>
2023-01-26 11:39:15 +0100

    Fix device info and node BasicInformation (#181)

--- matter_server/common/models/node.py
+++ matter_server/common/models/node.py
@@ -203,7 +203,9 @@ class MatterNode:
         return cast(
             str,
             self.get_attribute(
-                self.root_device_type_instance.endpoint, Clusters.Basic, "NodeLabel"
+                self.root_device_type_instance.endpoint,
+                Clusters.BasicInformation,
+                "NodeLabel",
             ).value,
         )
 
@@ -213,7 +215,9 @@ class MatterNode:
         return cast(
             str,
             self.get_attribute(
-                self.root_device_type_instance.endpoint, Clusters.Basic, "UniqueID"
+                self.root_device_type_instance.endpoint,
+                Clusters.BasicInformation,
+                "UniqueID",
             ).value,
         )
 
