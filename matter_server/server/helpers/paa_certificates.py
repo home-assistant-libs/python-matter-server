3c1a10c7ee78a1b05787609724f0181340c3dd72
Martin Hjelmare <marhje52@gmail.com>
2023-03-29 13:35:23 +0200

    Fix lint and test issues on main (#247)

--- matter_server/server/helpers/paa_certificates.py
+++ matter_server/server/helpers/paa_certificates.py
@@ -9,12 +9,14 @@ All rights reserved.
 
 import asyncio
 import logging
-import pathlib
+from os import makedirs
 import re
 
-from aiohttp import ClientSession, ClientError
+from aiohttp import ClientError, ClientSession
 from cryptography import x509
 from cryptography.hazmat.primitives import serialization
+
+from matter_server.server.const import PAA_ROOT_CERTS_DIR
 
 LOGGER = logging.getLogger(__name__)
 PRODUCTION_URL = "https://on.dcl.csa-iot.org"
@@ -23,16 +25,14 @@ TEST_URL = "https://on.test-net.dcl.csa-iot.org"
 LAST_CERT_IDS: set[str] = set()
 
 
-async def write_paa_root_cert(
-    certificate: str, subject: str, root_path: pathlib.Path
-) -> None:
+async def write_paa_root_cert(certificate: str, subject: str) -> None:
     """Write certificate from string to file."""
 
     def _write() -> None:
         filename_base = "dcld_mirror_" + re.sub(
             "[^a-zA-Z0-9_-]", "", re.sub("[=, ]", "_", subject)
         )
-        filepath_base = root_path.joinpath(filename_base)
+        filepath_base = PAA_ROOT_CERTS_DIR.joinpath(filename_base)
         # handle PEM certificate file
         file_path_pem = f"{filepath_base}.pem"
         LOGGER.debug("Writing certificate %s", file_path_pem)
@@ -50,12 +50,14 @@ async def write_paa_root_cert(
 
 
 async def fetch_certificates(
-    paa_trust_store_path: pathlib.Path,
     fetch_test_certificates: bool = True,
     fetch_production_certificates: bool = True,
 ) -> int:
     """Fetch PAA Certificates."""
     LOGGER.info("Fetching the latest PAA root certificates from DCL.")
+    if not PAA_ROOT_CERTS_DIR.is_dir():
+        loop = asyncio.get_running_loop()
+        await loop.run_in_executor(None, makedirs, PAA_ROOT_CERTS_DIR)
     fetch_count: int = 0
     base_urls = set()
     # determine which url's need to be queried.
@@ -94,7 +96,6 @@ async def fetch_certificates(
                     await write_paa_root_cert(
                         certificate,
                         subject,
-                        paa_trust_store_path,
                     )
                     LAST_CERT_IDS.add(paa["subjectKeyId"])
                     fetch_count += 1
