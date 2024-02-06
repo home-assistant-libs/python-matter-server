fdd4cc4dac6b9acefdb50356ee255fd7ef40c7b7
Stefan Agner <stefan@agner.ch>
2024-02-06 09:57:57 +0100

    Derive from BaseSite

--- matter_server/server/helpers/custom_web_runner.py
+++ matter_server/server/helpers/custom_web_runner.py
@@ -1,13 +1,14 @@
 """MultiHost capable aiohttp Site."""
 from __future__ import annotations
 
+import asyncio
 from ssl import SSLContext
 
 from aiohttp import web
 from yarl import URL
 
 
-class MultiHostTCPSite(web.TCPSite):
+class MultiHostTCPSite(web.BaseSite):
     """MultiHost capable aiohttp Site.
 
     Vanilla TCPSite accepts only str as host. However, the underlying asyncio's
@@ -15,6 +16,8 @@ class MultiHostTCPSite(web.TCPSite):
     host IP's. To support multiple server_host entries (e.g. to enable dual-stack
     explicitly), we would like to pass an array of strings.
     """
+
+    __slots__ = ("_host", "_port", "_reuse_address", "_reuse_port", "_hosturl")
 
     def __init__(
         self,
@@ -44,3 +47,19 @@ class MultiHostTCPSite(web.TCPSite):
         scheme = "https" if self._ssl_context else "http"
         host = self._host[0] if isinstance(self._host, list) else "0.0.0.0"
         return str(URL.build(scheme=scheme, host=host, port=self._port))
+
+    async def start(self) -> None:
+        """Start server."""
+        await super().start()
+        loop = asyncio.get_running_loop()
+        server = self._runner.server
+        assert server is not None
+        self._server = await loop.create_server(
+            server,
+            self._host,
+            self._port,
+            ssl=self._ssl_context,
+            backlog=self._backlog,
+            reuse_address=self._reuse_address,
+            reuse_port=self._reuse_port,
+        )
