41d9776c1645d052954940282fd47e2d57125597
Marcel van der Veldt <marcel.vanderveldt@nabucasa.com>
2024-02-09 15:07:22 +0100

    Fix for leading zero mismatch in fabric check (#544)

--- matter_server/server/device_controller.py
+++ matter_server/server/device_controller.py
@@ -1168,7 +1168,7 @@ class MatterDeviceController:
             return
         if service_type == MDNS_TYPE_OPERATIONAL_NODE:
             name = name.lower()
-            if not name.startswith(self.fabric_id_hex):
+            if self.fabric_id_hex not in name:
                 # filter out messages that are not for our fabric
                 return
             LOGGER.debug("Received %s MDNS event for %s", state_change, name)
