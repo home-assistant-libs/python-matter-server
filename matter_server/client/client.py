d432a0935ae861b155b29e9d1200934f8ef311ff
Marcel van der Veldt <m.vanderveldt@outlook.com>
2024-02-27 16:45:32 +0100

    uniform the result of read_attribute to always return a dict

--- matter_server/client/client.py
+++ matter_server/client/client.py
@@ -445,24 +445,26 @@ class MatterClient:
         self,
         node_id: int,
         attribute_path: str,
-    ) -> Any:
-        """Read attribute(s) on a node."""
-        return await self.send_command(
+    ) -> dict[str, Any]:
+        """Read one or more attribute(s) on a node by specifying an attributepath."""
+        updated_values = await self.send_command(
             APICommand.READ_ATTRIBUTE,
             require_schema=4,
             node_id=node_id,
             attribute_path=attribute_path,
         )
+        if not isinstance(updated_values, dict):
+            # can happen is the server is running schema < 8
+            return {attribute_path: updated_values}
+        return cast(dict[str, Any], updated_values)
 
     async def refresh_attribute(
         self,
         node_id: int,
         attribute_path: str,
-    ) -> Any:
+    ) -> None:
         """Read attribute(s) on a node and store the updated value(s)."""
         updated_values = await self.read_attribute(node_id, attribute_path)
-        if not isinstance(updated_values, dict):
-            updated_values = {attribute_path: updated_values}
         for attr_path, value in updated_values.items():
             self._nodes[node_id].update_attribute(attr_path, value)
 
