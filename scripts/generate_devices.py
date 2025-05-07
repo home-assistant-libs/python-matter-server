4f0c27c1581c94bc585710ac732675081fca4e9a
Stefan Agner <stefan@agner.ch>
2025-05-07 19:08:56 +0200

    Drop temporary workaround for Poll Control (#1145)

--- scripts/generate_devices.py
+++ scripts/generate_devices.py
@@ -113,9 +113,6 @@ class DeviceType:
                         # It's optional server cluster
                         or cluster["@serverLocked"] == "false"
                     )
-                    # Temporary: PollControl will be removed from matter_devices.xml
-                    # https://github.com/project-chip/connectedhomeip/pull/22718
-                    and cluster["@cluster"] != "Poll Control"
                 )
                 + ",}"  # extra comma to force black to do a cluster per line
             )
